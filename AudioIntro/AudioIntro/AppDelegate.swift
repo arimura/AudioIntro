c7a3723971a289ad76d4cdc46d603c690e75747d
arimura <kotaro_arimura@voyagegroup.com>
2021-11-08 13:30:38 +0900

    Set playback category

--- AudioIntro/AudioIntro/AppDelegate.swift
+++ AudioIntro/AudioIntro/AppDelegate.swift
@@ -6,13 +6,20 @@
 //
 
 import UIKit
+import AVFoundation
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        let audioSession = AVAudioSession.sharedInstance()
+           do {
+              try audioSession.setCategory(.playback)
+           } catch {
+               print("Setting category to AVAudioSessionCategoryPlayback failed.")
+           }
+        
         // Override point for customization after application launch.
         return true
     }
