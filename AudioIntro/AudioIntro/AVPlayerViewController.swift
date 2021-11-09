2d09055f2ba70785f3d7f2463a70ff479a1c9910
arimura <kotaro_arimura@voyagegroup.com>
2021-11-09 14:53:03 +0900

    Add http request on observer

--- AudioIntro/AudioIntro/AVPlayerViewController.swift
+++ AudioIntro/AudioIntro/AVPlayerViewController.swift
@@ -23,9 +23,20 @@ class AVPlayerViewController: UIViewController {
         playerItem.addObserver(self, forKeyPath: "status", options: NSKeyValueObservingOptions.new, context: nil)
         
         let timeScale = CMTimeScale(NSEC_PER_SEC)
-        let time = CMTime(seconds: 0.5, preferredTimescale: timeScale)
+        let time = CMTime(seconds: 2, preferredTimescale: timeScale)
         avPlayer.addPeriodicTimeObserver(forInterval: time, queue:DispatchQueue.main) {time in
-            print("hoge")
+            print("request")
+            //use local mock server
+            let url = URL(string: "http://192.168.86.218:8000")!
+
+            let task = URLSession.shared.dataTask(with: url) {(data, response, error) in
+                if error != nil{
+                    print("error: \(error!.localizedDescription)")
+                }
+                guard let data = data else { return }
+                print(String(data: data, encoding: .utf8)!)
+            }
+            task.resume()
         }
     }
     
